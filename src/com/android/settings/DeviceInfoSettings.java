ff643c781af09dbee3377fab748aeb0f1fee0d4f
Chirayu Desai <cdesai@cyanogenmod.org>
2012-12-26 11:33:38 +0530

    One does not simply become a Developer
    Also contains:
    From 2622e3cddd08448666fcfd584a8794727e1b2979 Mon Sep 17 00:00:00 2001
    From: Michael Bestas <mikeioannina@gmail.com>
    Date: Wed, 2 Jul 2014 16:32:03 +0300
    Subject: [PATCH] Fix translatability of modified dev countdown strings
    
    Change-Id: I3bc1749e27068a63144fd4f3b059396344e7858d

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -222,7 +222,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                     if (mDevHitToast != null) {
                         mDevHitToast.cancel();
                     }
-                    mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_on,
+                    mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_on_slim,
                             Toast.LENGTH_LONG);
                     mDevHitToast.show();
                     // This is good time to index the Developer Options
@@ -236,7 +236,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                         mDevHitToast.cancel();
                     }
                     mDevHitToast = Toast.makeText(getActivity(), getResources().getQuantityString(
-                            R.plurals.show_dev_countdown, mDevHitCountdown, mDevHitCountdown),
+                            R.plurals.show_dev_countdown_slim, mDevHitCountdown, mDevHitCountdown),
                             Toast.LENGTH_SHORT);
                     mDevHitToast.show();
                 }
@@ -244,7 +244,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                 if (mDevHitToast != null) {
                     mDevHitToast.cancel();
                 }
-                mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_already,
+                mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_already_slim,
                         Toast.LENGTH_LONG);
                 mDevHitToast.show();
             }

d569b2812c73ec6573a45ae8dd96fdc406786930
kufikugel <kufikugel@googlemail.com>
2012-12-26 09:08:25 +0100

    Settings: Add option to always show battery status on lockscreen (2/2)
    Based on the feature by burnsra in CM7
    
    Authored by blunden
    Tweaked by dvtonder
    slimed by kufikugel ;)
    
    Change-Id: I1c8c8eef3d0c913c621f735e7d5e395c8768fe76

--- src/com/android/settings/slim/LockscreenInterface.java
+++ src/com/android/settings/slim/LockscreenInterface.java
@@ -16,24 +16,30 @@
 package com.android.settings.slim;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class LockscreenInterface extends SettingsPreferenceFragment {
+public class LockscreenInterface extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
     private static final String TAG = "LockscreenInterface";
 
-    public boolean hasButtons() {
-        return !getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
-    }
+    private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
+
+    private ListPreference mBatteryStatus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.lockscreen_interface_settings);
+
+        mBatteryStatus = (ListPreference) findPreference(KEY_ALWAYS_BATTERY_PREF);
+        mBatteryStatus.setOnPreferenceChangeListener(this);
+        setBatteryStatusSummary();
     }
 
     @Override
@@ -44,10 +50,35 @@ public class LockscreenInterface extends SettingsPreferenceFragment {
     @Override
     public void onPause() {
         super.onPause();
+        setBatteryStatusSummary();
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mBatteryStatus) {
+            int value = Integer.valueOf((String) objValue);
+            int index = mBatteryStatus.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_ALWAYS_SHOW_BATTERY, value);
+            mBatteryStatus.setSummary(mBatteryStatus.getEntries()[index]);
+            return true;
+        }
+        return false;
+    }
+
+    private void setBatteryStatusSummary() {
+        // Set the battery status description text
+        if (mBatteryStatus != null) {
+            int batteryStatus = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_ALWAYS_SHOW_BATTERY, 0);
+            mBatteryStatus.setValueIndex(batteryStatus);
+            mBatteryStatus.setSummary(mBatteryStatus.getEntries()[batteryStatus]);
+        }
+    }
+
 }

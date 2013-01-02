16d54a7df6453bbff8c072e4b6ed2ac91a06eb87
tchaari <tarak.chaari@gmail.com>
2013-01-02 16:01:15 +0100

    Add long press action to Settings Tile (2/2)
    long press on Settings tile will open "Tiles and layout" Screen in Settings.apk
    long press on Autorotate tile -> ASS Display settings
    long press on lockscreen tile -> ASS Lockscreen interface
    
    Patchset 2:
    update also other longpress activities
    correction on settings title lockscreen
    
    Change-Id: If1f6736b18651e1da52dead31a32afb6023f80b0

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -838,4 +838,7 @@ public class Settings extends PreferenceActivity
     public static class ApnSettingsActivity extends Settings { /* empty */ }
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class ProfilesSettingsActivity extends Settings { /* empty */ }
+    public static class QuickSettingsTilesActivity extends Settings { /* empty */ }
+    public static class ASSDisplayActivity extends Settings { /* empty */ }
+    public static class ASSLockscreenActivity extends Settings { /* empty */ }
 }

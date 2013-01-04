0e0cf987404c5b6bcc7078b63d1d26806e8eb797
kufikugel <kufikugel@googlemail.com>
2013-01-04 05:04:31 +0100

    Settings: Code clean up and disable wifi country option
    if wifi is off
    
    Change-Id: I5fb697f47b2cfac771336f54a536ce91e3f77328

--- src/com/android/settings/slim/UserInterface.java
+++ src/com/android/settings/slim/UserInterface.java
@@ -16,59 +16,25 @@
 
 package com.android.settings.slim;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.util.Random;
-
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ActivityNotFoundException;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.graphics.Rect;
-import android.net.Uri;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.os.ServiceManager;
+import android.os.Handler;
 import android.os.SystemProperties;
+import android.net.wifi.WifiManager;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
-import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
-import android.provider.MediaStore;
 import android.provider.Settings;
-import android.text.Spannable;
-import android.view.Display;
-import android.view.IWindowManager;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.Window;
-import android.view.View;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
 import android.widget.Toast;
 
-import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
-import com.android.settings.util.CMDProcessor;
-import android.util.Log;
-import com.android.settings.util.Helpers;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class UserInterface extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
@@ -77,7 +43,6 @@ public class UserInterface extends SettingsPreferenceFragment implements OnPrefe
     private static final String MISC_SETTINGS = "misc";
     private static final String PREF_USE_ALT_RESOLVER = "use_alt_resolver";
     private static final String KEY_COUNTRY_CODE = "wifi_countrycode";
-    private static final String KEY_HARDWARE_KEYS = "hardware_keys";
 
     private Preference mLcdDensity;
     private CheckBoxPreference mUseAltResolver;
@@ -112,12 +77,31 @@ public class UserInterface extends SettingsPreferenceFragment implements OnPrefe
         } catch (Exception e) {
             getPreferenceScreen().removePreference(mLcdDensity);
         }
+        mLcdDensity.setSummary(getResources().getString(R.string.current_lcd_density) + currentProperty);
 
         mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 
         mCcodePref = (ListPreference) findPreference(KEY_COUNTRY_CODE);
+        mCcodePref.setOnPreferenceChangeListener(this);
+
+        updateWifiCodeSummary();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateWifiCodeSummary();
+    }
+
+    @Override
+    public void onPause() {
+        super.onResume();
+        updateWifiCodeSummary();
+    }
+
+    private void updateWifiCodeSummary() {
         if (mCcodePref != null) {
-            mCcodePref.setOnPreferenceChangeListener(this);
             String value = (mWifiManager.getCountryCode()).toUpperCase();
             if (value != null) {
                 mCcodePref.setValue(value);
@@ -125,20 +109,14 @@ public class UserInterface extends SettingsPreferenceFragment implements OnPrefe
             } else {
                 Log.e(TAG, "Failed to fetch country code");
             }
-        }
-
-        // Only show the hardware keys config on a device that does not have a navbar
-        IWindowManager windowManager = IWindowManager.Stub.asInterface(
-                ServiceManager.getService(Context.WINDOW_SERVICE));
-        try {
-            if (windowManager.hasNavigationBar()) {
-                mMisc.removePreference(findPreference(KEY_HARDWARE_KEYS));
+            if (mWifiManager.isWifiEnabled()) {
+                mCcodePref.setEnabled(true);
+            } else {
+                mCcodePref.setEnabled(false);
+                mCcodePref.setSummary(R.string.wifi_setting_countrycode_diabled);
             }
-        } catch (RemoteException e) {
-            // Do nothing
         }
 
-        mLcdDensity.setSummary(getResources().getString(R.string.current_lcd_density) + currentProperty);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -172,4 +150,5 @@ public class UserInterface extends SettingsPreferenceFragment implements OnPrefe
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
+
 }

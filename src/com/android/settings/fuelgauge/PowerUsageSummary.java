fcf1dc242783a2559e418500b02bdb3a70ecb992
jt1134 <jt1134@gmail.com>
2013-01-27 18:13:27 -0600

    PowerUsage: show more apps in battery statistics
    bump max items from 10 to 20
    
    Change-Id: I96c4ac11859e87b9a7ac0255318a7416b184a87b

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -99,7 +99,7 @@ public class PowerUsageSummary extends PreferenceFragment implements Runnable {
     private int mStatsType = BatteryStats.STATS_SINCE_CHARGED;
 
     private static final int MIN_POWER_THRESHOLD = 5;
-    private static final int MAX_ITEMS_TO_LIST = 10;
+    private static final int MAX_ITEMS_TO_LIST = 20;
 
     private long mStatsPeriod = 0;
     private double mMaxPower = 1;

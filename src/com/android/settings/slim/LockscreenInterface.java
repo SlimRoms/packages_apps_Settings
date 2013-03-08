2c3a690431fb9ed450cc986c3ca2266ef6b3d3a9
Lars Greiss <kufikugel@googlemail.com>
2013-03-08 14:10:07 +0100

    Fix possible NPE on largescreen devices
    On special conditions lockscreen interface caused NPE
    
    cheers
    
    Change-Id: I1240baec5b3f74fcc985daea2c9fcbc81769752a

--- src/com/android/settings/slim/LockscreenInterface.java
+++ src/com/android/settings/slim/LockscreenInterface.java
@@ -170,16 +170,14 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
         final int lockBeforeUnlock = Settings.Secure.getInt(getActivity().getContentResolver(),
                 Settings.Secure.LOCK_BEFORE_UNLOCK, 0);
 
+        PreferenceCategory sliderCategory = (PreferenceCategory) findPreference(KEY_SLIDER_OPTIONS);
+
         //setup custom lockscreen customize view
         if ((unsecureUnlockMethod != 1 && lockBeforeUnlock == 0)
                  || unsecureUnlockMethod == -1) {
-             PreferenceCategory sliderCategory = (PreferenceCategory) findPreference(KEY_SLIDER_OPTIONS);
              getPreferenceScreen().removePreference(sliderCategory);
-        }
-
-        if (!Utils.isPhone(getActivity())) {
+        } else if (!Utils.isPhone(getActivity())) {
              // Nothing for tablets and large screen devices
-             PreferenceCategory sliderCategory = (PreferenceCategory) findPreference(KEY_SLIDER_OPTIONS);
              sliderCategory.removePreference(mShortcuts);
              sliderCategory.removePreference(mLockscreenShortcutsLongpress);
              sliderCategory.removePreference(mLockscreenEightTargets);

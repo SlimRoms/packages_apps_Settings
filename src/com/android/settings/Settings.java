4e352586d58e2b6b212db725d8d0b7fd37d81fd0
fronti90 <m90braun@gmail.com>
2013-03-12 13:32:02 +0100

    changes for slimcenter
    ok it should work this way
    
    Change-Id: I57ba707aa28f44da8673369e2b13cfd40877dbc1

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -428,7 +428,7 @@ public class Settings extends PreferenceActivity
             // Ids are integers, so downcasting
             int id = (int) header.id;
             if (id == R.id.operator_settings || id == R.id.manufacturer_settings ||
-                    id == R.id.advanced_settings || id == R.id.slim_ota) {
+                    id == R.id.advanced_settings || id == R.id.slim_center) {
                 Utils.updateHeaderToSpecificActivityFromMetaDataOrRemove(this, target, header);
             } else if (id == R.id.wifi_settings) {
                 // Remove WiFi Settings if WiFi service is not available.

d0c5b132a599808788e522af07997f5d5a6a6c88
Lars Greiss <kufikugel@googlemail.com>
2013-05-24 10:28:33 +0200

    Settings: TRDS 2.0 (3/3)
    After testing with huge success we discovered here
    and there small problems (like NFC beam animation is broken,
    TRDS was disabled on cardock etc) which mainly was caused
    due that we used the already from AOSP introduced uiModes
    qualifier which could cause in special conditions this
    conflicts. Beside that we want a bit more flexibility.
    Result: This commit introduces the inverted qualifier as
    complete own qualifier undependent of the already AOSP
    used ones.
    
    Beside that it fixes all known issues......features:
    
    - it is now completly working over the AOSP Configuration classes
      Means every app get notice of a change trough onConfiguratioChange
      and restarts it's activity if change was detected.
    - app can disable that like some other configuration values
      over AndroidMainfest.xml
    - beside "inverted" folder another "notinverted" folder is introduced
      for another themeing (eg white)
    - qualifier order is direct before the official uiMode qualifiers
      (see AOSP docs)
    - All usual Configuration actions can be used with the inverted
      qualifiers....like BroadcastChange, direct read out from every
      app etc. etc. Note: Only system can change the uiInvertedMode
    
    So as you see a real own qualifier now
    
    NOTE: Apktool as well aapt need to be changed to fit the "new
    qualifier"....which means that we need to recompile our gapps
    that they react as well on this new behaviour. Think we will get them ready
    next 2 or 3 days
    
    Change-Id: I6ac22062e32be902ca834fc7ac2b12fd24e47d44

--- src/com/android/settings/slim/TRDSEnabler.java
+++ src/com/android/settings/slim/TRDSEnabler.java
@@ -56,7 +56,7 @@ public class TRDSEnabler implements CompoundButton.OnCheckedChangeListener {
 
     private void setSwitchState() {
         boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.UI_INVERTED_MODE, 0) == 1;
+                Settings.Secure.UI_INVERTED_MODE, 1) == 2;
         mStateMachineEvent = true;
         mSwitch.setChecked(enabled);
         mStateMachineEvent = false;
@@ -68,7 +68,7 @@ public class TRDSEnabler implements CompoundButton.OnCheckedChangeListener {
         }
         // Handle a switch change
         Settings.Secure.putInt(mContext.getContentResolver(),
-                Settings.Secure.UI_INVERTED_MODE, isChecked ? 1 : 0);
+                Settings.Secure.UI_INVERTED_MODE, isChecked ? 2 : 1);
 
         ActivityManager am = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
         List<ActivityManager.RunningAppProcessInfo> pids = am.getRunningAppProcesses();

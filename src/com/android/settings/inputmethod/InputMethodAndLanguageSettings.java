2300ac06c6b8178abef68d24f3a802beec71645a
Lars Greiss <kufikugel@googlemail.com>
2013-09-02 03:05:15 +0200

    Settings: fix NPE
    if the user resumes InputMethodAndLanguage settings and
    the user dictonary preference was already removed before,
    the user will run into a NPE.
    
    check it to fix it.
    
    Change-Id: I1eac844720768bb668e26f0a572c7876b7b0bcee

--- src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
+++ src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
@@ -206,6 +206,9 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
     }
 
     private void updateUserDictionaryPreference(Preference userDictionaryPreference) {
+        if (userDictionaryPreference == null) {
+            return;
+        }
         final Activity activity = getActivity();
         final TreeSet<String> localeList = UserDictionaryList.getUserDictionaryLocalesSet(activity);
         if (null == localeList) {

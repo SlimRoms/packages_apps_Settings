abd9e1a46a7b4d8534e5da217302237a76c223a6
Lars Greiss <kufikugel@googlemail.com>
2013-11-03 01:17:18 +0100

    Privacy Guard Slim version (2/2)
    Re-implements slims ehanced privacy guard from 4.3
    and adapt it to 4.4 changes
    
    Include:
      From: Lars Greiss <kufikugel@googlemail.com>
      Date: Sun, 16 Nov 2014 17:43:40 +0100
      Subject: [PATCH] privacy guard: fetch light image icons
    
      Change-Id: I4fae5b27a9dad297c48b9fba0e417fc50511f327
    
      From: Lars Greiss <kufikugel@googlemail.com>
      Date: Mon, 17 Nov 2014 15:01:30 +0100
      Subject: [PATCH] Settings: some fixes and addaptions to LP for PGM
    
       - add a reset icon - backup icon has changed which we used in the past
       - fix appops details call
       - reorder PGM position.
      Change-Id: I8de6da483651bc83a5f711a07633e1c3393a1611
    
    Change-Id: Ie87a72b4212e5a8063c1dfbf7fd759f3a14a0d12

--- src/com/android/settings/applications/AppOpsState.java
+++ src/com/android/settings/applications/AppOpsState.java
@@ -497,7 +497,7 @@ public class AppOpsState {
         final ArrayList<Integer> permOps = new ArrayList<Integer>();
         final int[] opToOrder = new int[AppOpsManager._NUM_OP];
         for (int i=0; i<tpl.ops.length; i++) {
-            if (tpl.showPerms[i]) {
+            if (i < tpl.showPerms.length && tpl.showPerms[i]) {
                 String perm = AppOpsManager.opToPermission(tpl.ops[i]);
                 if (perm != null && !perms.contains(perm)) {
                     perms.add(perm);

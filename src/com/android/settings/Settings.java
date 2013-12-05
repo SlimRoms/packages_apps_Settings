50fac8dbf900998d5edb0cfa39f4bbb9c584e6a1
Lars Greiss <kufikugel@googlemail.com>
2013-12-05 08:26:25 +0100

    settings: fix QuieHours call from fb
    Change-Id: I3245828aa675e186344d5370b9ea71ee6520349e

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -69,7 +69,6 @@ import com.android.settings.accounts.ManageAccountsSettings;
 import com.android.settings.applications.AppOpsSummary;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
-import com.android.settings.slim.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothEnabler;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.deviceinfo.Memory;
@@ -85,6 +84,8 @@ import com.android.settings.nfc.PaymentSettings;
 import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintServiceSettingsFragment;
 import com.android.settings.print.PrintSettingsFragment;
+import com.android.settings.slim.blacklist.BlacklistSettings;
+import com.android.settings.slim.QuietHours;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.vpn2.VpnSettings;
@@ -354,6 +355,7 @@ public class Settings extends PreferenceActivity
         PaymentSettings.class.getName(),
         KeyboardLayoutPickerFragment.class.getName(),
         BlacklistSettings.class.getName(),
+        QuietHours.class.getName(),
     };
 
     @Override

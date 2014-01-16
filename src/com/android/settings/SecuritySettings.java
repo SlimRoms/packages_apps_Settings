9374b927f9d070dcf493b5ab56c4b9ad298818d4
Lars Greiss <kufikugel@googlemail.com>
2014-01-16 19:24:33 +0100

    Settings: fix advanced reboot default value
    default value if 0 (off) in frameworks base. So reflect it here as well
    
    Change-Id: Ife09a4e9d74981c2ff5e74eb4977da5801a3969f

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -380,7 +380,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
 
         mAdvancedReboot = (ListPreference) root.findPreference(KEY_ADVANCED_REBOOT);
         mAdvancedReboot.setValue(String.valueOf(Settings.Secure.getInt(
-                getContentResolver(), Settings.Secure.ADVANCED_REBOOT, 1)));
+                getContentResolver(), Settings.Secure.ADVANCED_REBOOT, 0)));
         mAdvancedReboot.setSummary(mAdvancedReboot.getEntry());
         mAdvancedReboot.setOnPreferenceChangeListener(this);
 

4cec176f8ba7a672c81451673241c334c952896a
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-01-18 22:46:28 +0100

    Remove notification pulse and light for devices without LED
    PS2: if Light Options category is now empty, remove it
    
    Change-Id: Ib42f792faeffe8c454a5e1dcf7ee0970254189b0

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -154,6 +154,9 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                          e.printStackTrace();
                      }
                  }
+            } else {
+                 mLightOptions.removePreference(mNotificationPulse);
+                 mLightOptions.removePreference(mNotificationLight);
             }
 
             if (!getResources().getBoolean(
@@ -161,6 +164,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 mLightOptions.removePreference(mBatteryPulse);
             } else {
                 updateBatteryPulseDescription();
+            }
+
+            //If we're removed everything, get rid of the category
+            if (mLightOptions.getPreferenceCount() == 0) {
+                prefSet.removePreference(mLightOptions);
             }
         }
 

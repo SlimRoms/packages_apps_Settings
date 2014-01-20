c99103da42bc964f90f1bb4469696d3afcd82e93
Lars Greiss <kufikugel@googlemail.com>
2014-01-20 21:52:16 +0100

    Settings: fix Display settings FC on manta or devices without crt mode effect
    we removed the animations category for devices without crt mode effects. Result
    is the ListviewAnimation get now NPE on this devices due that they are in same category.
    
    Fix it with just not removing the category due that ListviewAnimations are always present
    and remove only the CRT mode option
    
    Change-Id: Iaf88e1c5a729a0fb3b7692b0b83b88c58dd1ee93

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -202,14 +202,16 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         PreferenceCategory animationOptions =
             (PreferenceCategory) prefSet.findPreference(KEY_ANIMATION_OPTIONS);
         mCrtMode = (ListPreference) prefSet.findPreference(KEY_POWER_CRT_MODE);
-        if (!electronBeamFadesConfig && mCrtMode != null) {
-            int crtMode = Settings.System.getInt(getContentResolver(),
-                    Settings.System.SYSTEM_POWER_CRT_MODE, 1);
-            mCrtMode.setValue(String.valueOf(crtMode));
-            mCrtMode.setSummary(mCrtMode.getEntry());
-            mCrtMode.setOnPreferenceChangeListener(this);
-        } else if (animationOptions != null) {
-            prefSet.removePreference(animationOptions);
+        if (mCrtMode != null) {
+            if (!electronBeamFadesConfig) {
+                int crtMode = Settings.System.getInt(getContentResolver(),
+                        Settings.System.SYSTEM_POWER_CRT_MODE, 1);
+                mCrtMode.setValue(String.valueOf(crtMode));
+                mCrtMode.setSummary(mCrtMode.getEntry());
+                mCrtMode.setOnPreferenceChangeListener(this);
+            } else if (animationOptions != null) {
+                animationOptions.removePreference(mCrtMode);
+            }
         }
 
         mListViewAnimation = (ListPreference) prefSet.findPreference(KEY_LISTVIEW_ANIMATION);

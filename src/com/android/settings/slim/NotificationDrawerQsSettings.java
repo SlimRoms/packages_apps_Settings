e4f02036ad8766d6eb342870dd3c76a97a006d45
Jubakuba <Jubakuba@gmail.com>
2014-02-12 03:13:42 -0700

    Settings: Notification Reminders (2/2)
    PS4: Undo Unintentional PS3 change
    
    Change-Id: I6be8d2267154c663a9977118f8e75e220cb7b9f6

--- src/com/android/settings/slim/NotificationDrawerQsSettings.java
+++ src/com/android/settings/slim/NotificationDrawerQsSettings.java
@@ -18,6 +18,9 @@ package com.android.settings.slim;
 
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
@@ -25,6 +28,7 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.RingtonePreference;
 import android.provider.Settings;
 import android.os.UserHandle;
 
@@ -44,6 +48,12 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
             "notification_hide_labels";
     private static final String PREF_NOTIFICATION_ALPHA =
             "notification_alpha";
+    private static final String PREF_NOTI_REMINDER_SOUND =
+            "noti_reminder_sound";
+    private static final String PREF_NOTI_REMINDER_ENABLED =
+            "noti_reminder_enabled";
+    private static final String PREF_NOTI_REMINDER_RINGTONE =
+            "noti_reminder_ringtone";
     private static final String PRE_QUICK_PULLDOWN =
             "quick_pulldown";
     private static final String PRE_SMART_PULLDOWN =
@@ -57,6 +67,9 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
 
     ListPreference mHideLabels;
     SeekBarPreference mNotificationAlpha;
+    CheckBoxPreference mReminder;
+    ListPreference mReminderMode;
+    RingtonePreference mReminderRingtone;
     ListPreference mQuickPulldown;
     ListPreference mSmartPulldown;
     CheckBoxPreference mCollapsePanel;
@@ -97,6 +110,35 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
         mNotificationAlpha = (SeekBarPreference) findPreference(PREF_NOTIFICATION_ALPHA);
         mNotificationAlpha.setInitValue((int) (transparency * 100));
         mNotificationAlpha.setOnPreferenceChangeListener(this);
+
+        mReminder = (CheckBoxPreference) findPreference(PREF_NOTI_REMINDER_ENABLED);
+        mReminder.setChecked(Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.REMINDER_ALERT_ENABLED, 0, UserHandle.USER_CURRENT) == 1);
+        mReminder.setOnPreferenceChangeListener(this);
+
+        mReminderMode = (ListPreference) findPreference(PREF_NOTI_REMINDER_SOUND);
+        int mode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.REMINDER_ALERT_NOTIFY, 0, UserHandle.USER_CURRENT);
+        mReminderMode.setValue(String.valueOf(mode));
+        mReminderMode.setOnPreferenceChangeListener(this);
+        updateReminderModeSummary(mode);
+
+        mReminderRingtone =
+                (RingtonePreference) findPreference(PREF_NOTI_REMINDER_RINGTONE);
+        Uri ringtone = null;
+        String ringtoneString = Settings.System.getStringForUser(getContentResolver(),
+                Settings.System.REMINDER_ALERT_RINGER, UserHandle.USER_CURRENT);
+        if (ringtoneString == null) {
+            // Value not set, defaults to Default Ringtone
+            ringtone = RingtoneManager.getDefaultUri(
+                    RingtoneManager.TYPE_RINGTONE);
+        } else {
+            ringtone = Uri.parse(ringtoneString);
+        }
+        Ringtone alert = RingtoneManager.getRingtone(getActivity(), ringtone);
+        mReminderRingtone.setSummary(alert.getTitle(getActivity()));
+        mReminderRingtone.setOnPreferenceChangeListener(this);
+        mReminderRingtone.setEnabled(mode != 0);
 
         mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
         mSmartPulldown = (ListPreference) findPreference(PRE_SMART_PULLDOWN);
@@ -180,6 +222,27 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
                     Settings.System.QS_COLLAPSE_PANEL,
                     (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
             return true;
+        } else if (preference == mReminder) {
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.REMINDER_ALERT_ENABLED,
+                    (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mReminderMode) {
+            int mode = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.REMINDER_ALERT_NOTIFY,
+                    mode, UserHandle.USER_CURRENT);
+            updateReminderModeSummary(mode);
+            mReminderRingtone.setEnabled(mode != 0);
+            return true;
+        } else if (preference == mReminderRingtone) {
+            Uri val = Uri.parse((String) newValue);
+            Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), val);
+            mReminderRingtone.setSummary(ringtone.getTitle(getActivity()));
+            Settings.System.putStringForUser(getContentResolver(),
+                    Settings.System.REMINDER_ALERT_RINGER,
+                    val.toString(), UserHandle.USER_CURRENT);
+            return true;
         }
         return false;
     }
@@ -231,5 +294,21 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
         text.append(" " + res.getString(R.string.notification_hide_labels_text));
 
         mHideLabels.setSummary(text.toString());
+    }
+
+    private void updateReminderModeSummary(int value) {
+        int resId;
+        switch (value) {
+            case 1:
+                resId = R.string.enabled;
+                break;
+            case 2:
+                resId = R.string.noti_reminder_sound_looping;
+                break;
+            default:
+                resId = R.string.disabled;
+                break;
+        }
+        mReminderMode.setSummary(getResources().getString(resId));
     }
 }

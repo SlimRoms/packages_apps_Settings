4917ec6d1750a0d2257d5bdbde1cfede96eef926
Jubakuba <Jubakuba@gmail.com>
2014-03-12 21:32:44 -0600

    Settings: More Lockscreen Colors (2/2)
    PS2: fix derp caught by Chandra Poerwanto
    
    Change-Id: I5469c42b4e944aab4b1b0c21d5ef41a8d1a1213a

--- src/com/android/settings/slim/LockscreenStyle.java
+++ src/com/android/settings/slim/LockscreenStyle.java
@@ -72,6 +72,11 @@ public class LockscreenStyle extends SettingsPreferenceFragment
             "lockscreen_lock_color";
     private static final String KEY_LOCKSCREEN_DOTS_COLOR =
             "lockscreen_dots_color";
+    private static final String KEY_LOCKSCREEN_TARGETS_COLOR =
+            "lockscreen_targets_color";
+    private static final String KEY_LOCKSCREEN_MISC_COLOR =
+            "lockscreen_misc_color";
+
 
     private String mDefault;
 
@@ -80,6 +85,8 @@ public class LockscreenStyle extends SettingsPreferenceFragment
     private ColorPickerPreference mFrameColor;
     private ColorPickerPreference mLockColor;
     private ColorPickerPreference mDotsColor;
+    private ColorPickerPreference mTargetsColor;
+    private ColorPickerPreference mMiscColor;
 
     private ListPreference mLockIcon;
 
@@ -152,6 +159,26 @@ public class LockscreenStyle extends SettingsPreferenceFragment
                 getResources().getString(
                 R.string.lockscreen_dots_color_summary), dotsColor);
         mDotsColor.setNewPreviewColor(dotsColor);
+
+        mTargetsColor = (ColorPickerPreference)
+                findPreference(KEY_LOCKSCREEN_TARGETS_COLOR);
+        mTargetsColor.setOnPreferenceChangeListener(this);
+        int targetColor = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCKSCREEN_TARGETS_COLOR, -2);
+        setPreferenceSummary(mTargetsColor,
+                getResources().getString(
+                R.string.lockscreen_targets_color_summary), targetColor);
+        mTargetsColor.setNewPreviewColor(targetColor);
+
+        mMiscColor = (ColorPickerPreference)
+                findPreference(KEY_LOCKSCREEN_MISC_COLOR);
+        mMiscColor.setOnPreferenceChangeListener(this);
+        int miscColor = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCKSCREEN_MISC_COLOR, -2);
+        setPreferenceSummary(mMiscColor,
+                getResources().getString(
+                R.string.lockscreen_misc_color_summary), miscColor);
+        mMiscColor.setNewPreviewColor(miscColor);
 
         // No lock-slider is available
         boolean dotsDisabled = new LockPatternUtils(getActivity()).isSecure()
@@ -259,6 +286,20 @@ public class LockscreenStyle extends SettingsPreferenceFragment
                     Settings.Secure.LOCKSCREEN_DOTS_COLOR, val);
             setPreferenceSummary(preference,
                     getResources().getString(R.string.lockscreen_dots_color_summary), val);
+            return true;
+        } else if (preference == mTargetsColor) {
+            int val = Integer.valueOf(String.valueOf(newValue));
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.LOCKSCREEN_TARGETS_COLOR, val);
+            setPreferenceSummary(preference,
+                    getResources().getString(R.string.lockscreen_targets_color_summary), val);
+            return true;
+        } else if (preference == mMiscColor) {
+            int val = Integer.valueOf(String.valueOf(newValue));
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.LOCKSCREEN_MISC_COLOR, val);
+            setPreferenceSummary(preference,
+                    getResources().getString(R.string.lockscreen_misc_color_summary), val);
             return true;
         }
         return false;
@@ -410,6 +451,10 @@ public class LockscreenStyle extends SettingsPreferenceFragment
                                     Settings.Secure.LOCKSCREEN_LOCK_COLOR, -2);
                             Settings.Secure.putInt(getActivity().getContentResolver(),
                                     Settings.Secure.LOCKSCREEN_DOTS_COLOR, -2);
+                            Settings.Secure.putInt(getActivity().getContentResolver(),
+                                    Settings.Secure.LOCKSCREEN_TARGETS_COLOR, -2);
+                            Settings.Secure.putInt(getActivity().getContentResolver(),
+                                    Settings.Secure.LOCKSCREEN_MISC_COLOR, -2);
                             getOwner().createCustomView();
                         }
                     })

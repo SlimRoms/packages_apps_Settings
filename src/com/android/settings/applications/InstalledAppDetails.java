b5eacf0b6a89b8531cbbb8693ec2e096d719eeba
Lars Greiss <kufikugel@googlemail.com>
2014-06-30 19:04:22 +0200

    Settings: Heads up meets SlimKat....SlimKat meets Google IO [4/4]
    For detailed explanation see here: https://gerrit.slimroms.net/#/c/9937/
    
    This patch adds the 4 possible user configurable settings.
    
    1. Enable always expanded heads up notifications.
    2. Set heads up snooze time
    3. Set timout timer
    4. Show updates of pending notifications
    
    5. Add in app info screen a checkbox to enable heads up for the app.
       As another possibility aside the longpress on the notification on
       notification drawer.
    
    Change-Id: I999df799cecb487c502e4a77626445813eeb197f

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -143,6 +143,7 @@ public class InstalledAppDetails extends Fragment
     private Button mClearDataButton;
     private Button mMoveAppButton;
     private CompoundButton mNotificationSwitch;
+    private CompoundButton mHeadsUpSwitch;
     private Button mAppOpsButton;
 
     private PackageMoveObserver mPackageMoveObserver;
@@ -418,6 +419,17 @@ public class InstalledAppDetails extends Fragment
         }
     }
 
+    private void initHeadsUpButton() {
+        boolean enabled = mPm.getHeadsUpSetting(mAppEntry.info.packageName);
+        mHeadsUpSwitch.setChecked(enabled);
+        if (isThisASystemPackage() || !mNotificationSwitch.isChecked()) {
+            mHeadsUpSwitch.setEnabled(false);
+        } else {
+            mHeadsUpSwitch.setEnabled(true);
+            mHeadsUpSwitch.setOnCheckedChangeListener(this);
+        }
+    }
+
     private void initAppOpsButton() {
         boolean enabled = true;
         if (isThisASystemPackage()) {
@@ -507,6 +519,8 @@ public class InstalledAppDetails extends Fragment
         mEnableCompatibilityCB = (CheckBox)view.findViewById(R.id.enable_compatibility_cb);
         
         mNotificationSwitch = (CompoundButton) view.findViewById(R.id.notification_switch);
+
+        mHeadsUpSwitch = (CompoundButton) view.findViewById(R.id.heads_up_switch);
 
         mAppOps = (AppOpsManager) getActivity().getSystemService(Context.APP_OPS_SERVICE);
         mAppOpsButton = (Button) view.findViewById(R.id.app_ops_button);
@@ -918,6 +932,10 @@ public class InstalledAppDetails extends Fragment
             }
         }
 
+        // only setup heads up if we didn't get uninstalled
+        if (!mMoveInProgress) {
+            initHeadsUpButton();
+        }
         return true;
     }
 
@@ -1245,6 +1263,10 @@ public class InstalledAppDetails extends Fragment
                         public void onClick(DialogInterface dialog, int which) {
                             // Re-enable the checkbox
                             getOwner().mNotificationSwitch.setChecked(true);
+                            // Give access to heads up check box.
+                            if (getOwner().mHeadsUpSwitch != null) {
+                                getOwner().mHeadsUpSwitch.setEnabled(true);
+                            }
                         }
                     })
                     .create();
@@ -1347,8 +1369,14 @@ public class InstalledAppDetails extends Fragment
         try {
             final boolean enable = mNotificationSwitch.isChecked();
             nm.setNotificationsEnabledForPackage(packageName, mAppEntry.info.uid, enabled);
+            if (mHeadsUpSwitch != null) {
+                mHeadsUpSwitch.setEnabled(enable);
+            }
         } catch (android.os.RemoteException ex) {
             mNotificationSwitch.setChecked(!enabled); // revert
+            if (mHeadsUpSwitch != null) {
+                mHeadsUpSwitch.setEnabled(!enabled);
+            }
         }
     }
 
@@ -1454,6 +1482,8 @@ public class InstalledAppDetails extends Fragment
             } else {
                 setNotificationsEnabled(true);
             }
+        } else if (buttonView == mHeadsUpSwitch) {
+            mPm.setHeadsUpSetting(packageName, isChecked);
         }
     }
 }

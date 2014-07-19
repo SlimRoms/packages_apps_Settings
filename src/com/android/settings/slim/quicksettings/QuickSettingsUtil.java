0b824bd059846ac87d97ccab6239eecb327cb8da
Lars Greiss <kufikugel@googlemail.com>
2014-07-19 23:43:26 +0200

    settings: add remote view tile (2/2)
    Change-Id: If1459085628fecd8875cca51609b6bc7efbf1cbe

--- src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -56,6 +56,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFI;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
+import static com.android.internal.util.slim.QSConstants.TILE_REMOTEDISPLAY;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -237,6 +238,10 @@ public class QuickSettingsUtil {
         if (!DeviceUtils.deviceSupportsUsbTether(context)) {
             DYNAMIC_TILES_DEFAULT.remove(TILE_USBTETHER);
         }
+        // Don't show the remote display tile if not supported
+        if (!DeviceUtils.deviceSupportsRemoteDisplay(context)) {
+            DYNAMIC_TILES_DEFAULT.remove(TILE_REMOTEDISPLAY);
+        }
         return DYNAMIC_TILES_DEFAULT;
     }
 
@@ -249,6 +254,8 @@ public class QuickSettingsUtil {
             return context.getResources().getString(R.string.dynamic_tile_alarm);
         } else if (tile.equals(TILE_BUGREPORT)) {
             return context.getResources().getString(R.string.dynamic_tile_bugreport);
+        } else if (tile.equals(TILE_REMOTEDISPLAY)) {
+            return context.getResources().getString(R.string.dynamic_tile_remote_display);
         }
         return null;
     }

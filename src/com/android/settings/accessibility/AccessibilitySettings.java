51c7bf1c3411172ed2eee8c69038e6f121091e95
dankoman <dankoman30@gmail.com>
2014-08-25 14:40:14 +0200

    Settings: Slimrecents card/text color [1/2]
    Allows user to set custom colors for Slimrecents card
        header text and background.
    
    PS: fix strings
    
    PS: move recent preferences into separate
            subcategory under Interface settings (squashed).
    Change-Id: I6cd62c14f0d2b1aba54efa7dfa0c8a3db7669a9e

--- src/com/android/settings/accessibility/AccessibilitySettings.java
+++ src/com/android/settings/accessibility/AccessibilitySettings.java
@@ -44,12 +44,8 @@ import android.provider.Settings;
 import android.text.TextUtils;
 import android.text.TextUtils.SimpleStringSplitter;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.accessibility.AccessibilityManager;
 import android.widget.TextView;
@@ -61,8 +57,6 @@ import com.android.settings.DialogCreatable;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import java.util.HashSet;
 import java.util.List;
@@ -109,18 +103,8 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
             "captioning_preference_screen";
     private static final String DISPLAY_MAGNIFICATION_PREFERENCE_SCREEN =
             "screen_magnification_preference_screen";
-    private static final String RECENT_PANEL_SHOW_TOPMOST =
-            "recent_panel_show_topmost";
     private static final String SHAKE_SENSITIVITY =
             "shake_sensitivity";
-    private static final String RECENT_PANEL_LEFTY_MODE =
-            "recent_panel_lefty_mode";
-    private static final String RECENT_PANEL_SCALE =
-            "recent_panel_scale";
-    private static final String RECENT_PANEL_EXPANDED_MODE =
-            "recent_panel_expanded_mode";
-    private static final String RECENT_PANEL_BG_COLOR =
-            "recent_panel_bg_color";
 
     // Extras passed to sub-fragments.
     static final String EXTRA_PREFERENCE_KEY = "preference_key";
@@ -217,14 +201,6 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
     private PreferenceScreen mCaptioningPreferenceScreen;
     private PreferenceScreen mDisplayMagnificationPreferenceScreen;
     private PreferenceScreen mGlobalGesturePreferenceScreen;
-    private CheckBoxPreference mRecentsShowTopmost;
-    private CheckBoxPreference mRecentPanelLeftyMode;
-    private ListPreference mRecentPanelScale;
-    private ListPreference mRecentPanelExpandedMode;
-    private ColorPickerPreference mRecentPanelBgColor;
-
-    private static final int MENU_RESET = Menu.FIRST;
-    private static final int DEFAULT_BACKGROUND_COLOR = 0x00ffffff;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -271,39 +247,6 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
             int value = Integer.parseInt((String) newValue);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.SHAKE_SENSITIVITY, value);
-            return true;
-        } else if (preference == mRecentPanelScale) {
-            int value = Integer.parseInt((String) newValue);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.RECENT_PANEL_SCALE_FACTOR, value);
-            return true;
-        } else if (preference == mRecentPanelExpandedMode) {
-            int value = Integer.parseInt((String) newValue);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.RECENT_PANEL_EXPANDED_MODE, value);
-            return true;
-        } else if (preference == mRecentPanelBgColor) {
-            String hex = ColorPickerPreference.convertToARGB(
-                    Integer.valueOf(String.valueOf(newValue)));
-            if (hex.equals("#00ffffff")) {
-                preference.setSummary(R.string.trds_default_color);
-            } else {
-                preference.setSummary(hex);
-            }
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.RECENT_PANEL_BG_COLOR,
-                    intHex);
-            return true;
-        } else if (preference == mRecentPanelLeftyMode) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.RECENT_PANEL_GRAVITY,
-                    ((Boolean) newValue) ? Gravity.LEFT : Gravity.RIGHT);
-            return true;
-        } else if (preference == mRecentsShowTopmost) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.RECENT_PANEL_SHOW_TOPMOST,
-                    ((Boolean) newValue) ? 1 : 0);
             return true;
         }
         return false;
@@ -417,21 +360,6 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
                 (ListPreference) findPreference(SHAKE_SENSITIVITY);
         mShakeSensitivity.setOnPreferenceChangeListener(this);
 
-        // Recent panel background color
-        mRecentPanelBgColor =
-                (ColorPickerPreference) findPreference(RECENT_PANEL_BG_COLOR);
-        mRecentPanelBgColor.setOnPreferenceChangeListener(this);
-        final int intColor = Settings.System.getInt(getContentResolver(),
-                Settings.System.RECENT_PANEL_BG_COLOR, 0x00ffffff);
-        String hexColor = String.format("#%08x", (0x00ffffff & intColor));
-        if (hexColor.equals("#00ffffff")) {
-            mRecentPanelBgColor.setSummary(R.string.trds_default_color);
-        } else {
-            mRecentPanelBgColor.setSummary(hexColor);
-        }
-        mRecentPanelBgColor.setNewPreviewColor(intColor);
-        setHasOptionsMenu(true);
-
         // Long press timeout.
         mSelectLongPressTimeoutPreference =
                 (SlimSeekBarPreference) findPreference(SELECT_LONG_PRESS_TIMEOUT_PREFERENCE);
@@ -462,63 +390,6 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
             // nor long press power does not show global actions menu.
             mSystemsCategory.removePreference(mGlobalGesturePreferenceScreen);
         }
-
-        boolean enableRecentsShowTopmost = Settings.System.getInt(getContentResolver(),
-                                      Settings.System.RECENT_PANEL_SHOW_TOPMOST, 0) == 1;
-        mRecentsShowTopmost = (CheckBoxPreference) findPreference(RECENT_PANEL_SHOW_TOPMOST);
-        mRecentsShowTopmost.setChecked(enableRecentsShowTopmost);
-        mRecentsShowTopmost.setOnPreferenceChangeListener(this);
-
-        mRecentPanelLeftyMode =
-                (CheckBoxPreference) findPreference(RECENT_PANEL_LEFTY_MODE);
-        mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
-
-        mRecentPanelScale =
-                (ListPreference) findPreference(RECENT_PANEL_SCALE);
-        mRecentPanelScale.setOnPreferenceChangeListener(this);
-
-        mRecentPanelExpandedMode =
-                (ListPreference) findPreference(RECENT_PANEL_EXPANDED_MODE);
-        mRecentPanelExpandedMode.setOnPreferenceChangeListener(this);
-
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(0, MENU_RESET, 0, R.string.reset_default_message)
-                .setIcon(R.drawable.ic_settings_backup)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET:
-                resetToDefault();
-                return true;
-            default:
-                return super.onContextItemSelected(item);
-        }
-    }
-
-    private void resetToDefault() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
-        alertDialog.setTitle(R.string.shortcut_action_reset);
-        alertDialog.setMessage(R.string.qs_style_reset_message);
-        alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                resetValues();
-            }
-        });
-        alertDialog.setNegativeButton(R.string.cancel, null);
-        alertDialog.create().show();
-    }
-
-    private void resetValues() {
-        Settings.System.putInt(getContentResolver(),
-                Settings.System.RECENT_PANEL_BG_COLOR, DEFAULT_BACKGROUND_COLOR);
-        mRecentPanelBgColor.setNewPreviewColor(DEFAULT_BACKGROUND_COLOR);
-        mRecentPanelBgColor.setSummary(R.string.trds_default_color);
     }
 
     private void updateAllPreferences() {
@@ -684,18 +555,6 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
             mGlobalGesturePreferenceScreen.setSummary(
                     R.string.accessibility_global_gesture_preference_summary_off);
         }
-
-        final boolean recentLeftyMode = Settings.System.getInt(getContentResolver(),
-                Settings.System.RECENT_PANEL_GRAVITY, Gravity.RIGHT) == Gravity.LEFT;
-        mRecentPanelLeftyMode.setChecked(recentLeftyMode);
-
-        final int recentScale = Settings.System.getInt(getContentResolver(),
-                Settings.System.RECENT_PANEL_SCALE_FACTOR, 100);
-        mRecentPanelScale.setValue(recentScale + "");
-
-        final int recentExpandedMode = Settings.System.getInt(getContentResolver(),
-                Settings.System.RECENT_PANEL_EXPANDED_MODE, 0);
-        mRecentPanelExpandedMode.setValue(recentExpandedMode + "");
 
     }
 

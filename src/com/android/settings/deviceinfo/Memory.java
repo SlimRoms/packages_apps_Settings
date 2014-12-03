3b912a023eba88a59fb2a381d8bf1a7618877fd4
Lars Greiss <kufikugel@googlemail.com>
2014-12-03 19:22:34 +0100

    Settings: fix a couple of multiuser bugs
    - show interface settings
    - hide media scan on boot behaviour
    - show privacy guard
    
    for other users
    
    Change-Id: I2b488f69b820f289d7ddb04e5171637b355dc5cf

--- src/com/android/settings/deviceinfo/Memory.java
+++ src/com/android/settings/deviceinfo/Memory.java
@@ -34,6 +34,7 @@ import android.os.Environment;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.storage.IMountService;
 import android.os.storage.StorageEventListener;
@@ -41,6 +42,7 @@ import android.os.storage.StorageManager;
 import android.os.storage.StorageVolume;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
@@ -73,6 +75,7 @@ public class Memory extends SettingsPreferenceFragment
 
     private static final String TAG_CONFIRM_CLEAR_CACHE = "confirmClearCache";
     private static final String PREF_MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
+    private static final String PREF_OPTIONS_CATEGORY = "settings_options_prefs";
 
     private static final int DLG_CONFIRM_UNMOUNT = 1;
     private static final int DLG_ERROR_UNMOUNT = 2;
@@ -115,10 +118,17 @@ public class Memory extends SettingsPreferenceFragment
         }
 
         mMsob = (ListPreference) findPreference(PREF_MEDIA_SCANNER_ON_BOOT);
-        mMsob.setValue(String.valueOf(Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.MEDIA_SCANNER_ON_BOOT, 0)));
-        mMsob.setSummary(mMsob.getEntry());
-        mMsob.setOnPreferenceChangeListener(this);
+        if (UserHandle.myUserId() != UserHandle.USER_OWNER) {
+            PreferenceCategory options =
+                    (PreferenceCategory) findPreference(PREF_OPTIONS_CATEGORY);
+            getPreferenceScreen().removePreference(options);
+        } else {
+            mMsob.setValue(String.valueOf(
+                    Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.MEDIA_SCANNER_ON_BOOT, 0)));
+            mMsob.setSummary(mMsob.getEntry());
+            mMsob.setOnPreferenceChangeListener(this);
+        }
 
         setHasOptionsMenu(true);
     }

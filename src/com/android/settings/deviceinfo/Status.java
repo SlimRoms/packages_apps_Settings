90e3375d99a5ab81e41d32857606965fb927e082
Marcos Marado <mmarado@cyngn.com>
2015-01-13 16:48:04 +0000

    MSIM: behave as single SIM when appropriate
    On 'About phone' multi-SIM devices show network info for each SIM
    slot even if there's no SIM available. With this change, if the
    device has less than two SIMs it behaves as a single SIM device.
    
    Change-Id: Id45b8f7b1b3f7d47b882e49ccaf35ac28561a57f

--- src/com/android/settings/deviceinfo/Status.java
+++ src/com/android/settings/deviceinfo/Status.java
@@ -44,6 +44,7 @@ import android.telephony.PhoneNumberUtils;
 import android.telephony.PhoneStateListener;
 import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
+import android.telephony.SubscriptionManager;
 import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
@@ -665,7 +666,7 @@ public class Status extends PreferenceActivity {
     }
 
     private boolean isMultiSimEnabled() {
-        return (TelephonyManager.getDefault().getPhoneCount() > 1);
+        return (SubscriptionManager.getActiveSubInfoCount() > 1);
     }
 
     @Override

sooorajjj/android_packages_apps_Settings

AppOps: Add support for nfc operation
Add support for nfc operation

Change-Id: I0ca617b11d3f65f80cc49c3f629312e2fee1ba91

src/com/android/settings/applications/AppOpsState.java
@@ -197,8 +197,10 @@ public class AppOpsState {
                     AppOpsManager.OP_WAKE_LOCK,
                     AppOpsManager.OP_WIFI_CHANGE,
                     AppOpsManager.OP_BLUETOOTH_CHANGE,
-                    AppOpsManager.OP_DATA_CONNECT_CHANGE },
+                    AppOpsManager.OP_DATA_CONNECT_CHANGE,
+                    AppOpsManager.OP_NFC_CHANGE },
             new boolean[] { false,
+                    true,
                     true,
                     true,
                     true,

SlimRoms/packages_apps_Settings

src/com/android/settings/applications/AppOpsState.java
@@ -157,8 +157,10 @@ public class AppOpsState {
     public static final OpsTemplate DEVICE_TEMPLATE = new OpsTemplate(
             new int[] { AppOpsManager.OP_CALL_PHONE,
                     AppOpsManager.OP_WIFI_CHANGE,
-                    AppOpsManager.OP_BLUETOOTH_CHANGE },
+                    AppOpsManager.OP_BLUETOOTH_CHANGE,
+                    AppOpsManager.OP_NFC_CHANGE },
             new boolean[] { true,
+                    true,
                     true,
                     true }
             );
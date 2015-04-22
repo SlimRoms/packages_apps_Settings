daf9c1dadb0ac0aba8bfaebed6cb378bf883bc85
droyd <mikew8978@gmail.com>
2015-04-22 10:36:52 -0400

    Expose settings icons for tint
    no icon left behind
    
    This patch will ensure that no icon goes without theme and will allow Themes to define one color code in colors.xml to colorize all settings icons
    
    ROMS:
    when adding a new icon to your rom just follow the setup
    add your icon
    add your file
    settings calls the xml the xml calls the icon and color.
    
    Themes:
    simply change the color code in layers_colors.xml I added to colorize stock icons
    If you dont want to or cant tint your custom just add "exposed" to the icon name
    EX: ic_settings_display_exposed (NO TINT CALLED)
    
    Conflicts:
    	res/values/colors.xml
    	res/xml/dashboard_categories.xml
    
    Change-Id: I97ac5d612b94a383b2063f23e3933ed8e0c89556

--- src/com/android/settings/fuelgauge/BatteryEntry.java
+++ src/com/android/settings/fuelgauge/BatteryEntry.java
@@ -154,11 +154,11 @@ public class BatteryEntry {
                 break;
             case SCREEN:
                 name = context.getResources().getString(R.string.power_screen);
-                iconId = R.drawable.ic_settings_display;
+                iconId = R.drawable.ic_settings_display_exposed;
                 break;
             case FLASHLIGHT:
                 name = context.getResources().getString(R.string.power_flashlight);
-                iconId = R.drawable.ic_settings_display;
+                iconId = R.drawable.ic_settings_display_exposed;
                 break;
             case APP:
                 name = sipper.packageWithHighestDrain;

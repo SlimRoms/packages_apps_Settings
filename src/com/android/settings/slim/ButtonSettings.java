928fd0e7aa84c469ca6420b16d99b040a27a3e8a
Randall Rushing <randall.rushing@gmail.com>
2015-04-30 12:50:14 -0700

    Navigation: Bring up new Slim navigation and buttons frameworks (2 of 2)
          **  Action binding: A large variety of system actions, packages, and any
              resolvable shortcut and more. Action framework is used across all
              navigation implementations for a consistent UX
    
          **  Navigation bar and softkeys: Longpress and doubletap actions, force
              show menu button, longpress timeout, bar size. There is definately
              more to do here but this is a solid base.
    
          **  Introduce NX Smart Gesture Interface from the TeamEos project.
              NX is a gesture based navigation implementation.
              Current capabilities include:
    
                -- long and short right and left flings
                -- single tap, double tap, and long press right and left
                   making a current total of 10 assignable actions
                -- NxPulse: bar becomes a music visualizer during track playback
                -- indicator animation and Ripple on touch events
                -- Rock solid stable and under continuous development
    
          **  Hardware buttons: single tap, double tap, and longpress. Currently,
              back and home single taps are static and can not be changed. They can
              be made configurable if the demand is there. Force show navigation bar
              is also available
    
          **  All navigation implementations are multiuser supported (needs testing)
    
          **  Security: All navigation implentations enforce keyguard security and
              screen pinning.
    
          ** CREDITS AND RESPECT
    
             @cyanogenmod for basic utilities and hardware button masking
    
    Change-Id: I2c8afbb7facbe7d91e4f380838beee671ecc306f

---
+++ src/com/android/settings/slim/ButtonSettings.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2014 TeamEos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+
+public class ButtonSettings extends ActionFragment {
+    private static final String TAG = ButtonSettings.class.getSimpleName();
+
+    // category keys
+    private static final String CATEGORY_BACK = "back_key";
+    private static final String CATEGORY_HOME = "home_key";
+    private static final String CATEGORY_MENU = "menu_key";
+    private static final String CATEGORY_ASSIST = "assist_key";
+    private static final String CATEGORY_APPSWITCH = "app_switch_key";
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    private static final int KEY_MASK_HOME = 0x01;
+    private static final int KEY_MASK_BACK = 0x02;
+    private static final int KEY_MASK_MENU = 0x04;
+    private static final int KEY_MASK_ASSIST = 0x08;
+    private static final int KEY_MASK_APP_SWITCH = 0x10;
+    private static final int KEY_MASK_CAMERA = 0x20;
+    private static final int KEY_MASK_VOLUME = 0x40;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.button_settings);
+
+        ActionBar bar = getActivity().getActionBar();
+        if (bar != null) {
+            bar.setTitle(R.string.navigation_buttons);
+        }
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        final int deviceKeys = getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
+        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+
+        final PreferenceCategory backCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_BACK);
+        final PreferenceCategory homeCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_HOME);
+        final PreferenceCategory menuCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_MENU);
+        final PreferenceCategory assistCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_ASSIST);
+        final PreferenceCategory appSwitchCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_APPSWITCH);
+
+        if (!hasBackKey) prefScreen.removePreference(backCategory);
+        if (!hasHomeKey) prefScreen.removePreference(homeCategory);
+        if (!hasAppSwitchKey) prefScreen.removePreference(appSwitchCategory);
+        if (!hasMenuKey) prefScreen.removePreference(menuCategory);
+        if (!hasAssistKey) prefScreen.removePreference(assistCategory);
+
+        onPreferenceScreenLoaded();
+
+    }
+}

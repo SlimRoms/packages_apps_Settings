0320d61cdb885975799c7c3ad392727bf02a8ba7
Josue Rivera <prbassplayer@gmail.com>
2015-05-01 14:17:03 -0600

    Privacy Guard: Add option to disable notification (2/2)
    Change-Id: Ic6404be7c36af9074a9402a8bb350b5a157446da
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- src/com/android/settings/privacyguard/PrivacyGuardPrefs.java
+++ src/com/android/settings/privacyguard/PrivacyGuardPrefs.java
@@ -40,8 +40,10 @@ public class PrivacyGuardPrefs extends SettingsPreferenceFragment implements
     private static final String TAG = "PrivacyGuardPrefs";
 
     private static final String KEY_PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+    private static final String KEY_PRIVACY_GUARD_NOTIFICATION = "privacy_guard_notification";
 
     private SwitchPreference mPrivacyGuardDefault;
+    private SwitchPreference mPrivacyGuardNotification;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,6 +57,17 @@ public class PrivacyGuardPrefs extends SettingsPreferenceFragment implements
 
         mPrivacyGuardDefault.setChecked(Settings.Secure.getInt(getContentResolver(),
                 Settings.Secure.PRIVACY_GUARD_DEFAULT, 0) == 1);
+
+        mPrivacyGuardNotification =
+            (SwitchPreference) findPreference(KEY_PRIVACY_GUARD_NOTIFICATION);
+        mPrivacyGuardNotification.setOnPreferenceChangeListener(this);
+
+        try {
+            mPrivacyGuardNotification.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_NOTIFICATION) == 1);
+        } catch (SettingNotFoundException e) {
+            mPrivacyGuardNotification.setChecked(true);
+        }
     }
 
     @Override
@@ -75,6 +88,11 @@ public class PrivacyGuardPrefs extends SettingsPreferenceFragment implements
             boolean value = (Boolean) newValue;
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.PRIVACY_GUARD_DEFAULT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPrivacyGuardNotification) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_NOTIFICATION, value ? 1 : 0);
             return true;
         }
         return false;

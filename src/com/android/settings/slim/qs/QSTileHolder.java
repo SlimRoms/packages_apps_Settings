DesolationRom-legacy/android_packages_apps_Settings

QS: add Screenshot tile (2/2)
Change-Id: Ieb3eddd45be2a8c6c4bf19b5931c40998794324d

Conflicts:
	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java

src/com/android/settings/desolation/qs/QSTileHolder.java
@@ -113,6 +113,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_lte_on";
                 stringId = R.string.qs_tile_lte;
                 break;
+            case QSConstants.TILE_SCREENSHOT:
+                resourceName = "ic_qs_screenshot";
+                stringId = R.string.qs_screenshot_tile;
+                break;
             case QSConstants.TILE_NFC:
                 resourceName = "ic_qs_nfc_on";
                 stringId = R.string.qs_tile_nfc;

SlimRoms/packages_apps_Settings

Change-Id: I6584c734a29e729dacf35f9f1ffed9efb6197279

src/com/android/settings/slim/qs/QSTileHolder.java
@@ -149,6 +149,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_ambientdisplay_on";
                 stringId = R.string.qs_tile_ambient_display;
                 break;
+            case QSConstants.TILE_SCREENSHOT:
+                resourceName = "ic_qs_screenshot";
+                stringId = R.string.qs_tile_screenshot;
+                break;
             default:
                 return null;
         }
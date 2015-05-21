28f86863685289f19c161e8fa279d3b69dcc8db1
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-05-21 21:19:18 +0200

    QS: expanded desktop tile [2/2]
    Change-Id: I1db8bfe71f6293cbd164f9e258a93415407a61f5
    PS: adapted to the expanded desktop entry moved into the Interface Settings

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -101,5 +101,7 @@ public class Settings extends SettingsActivity {
 
     public static class TopLevelSettings extends SettingsActivity { /* empty */ }
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class QSTilesSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
 }
 

101b984ce29c0f702d3cfad6c76e024727dcad80
ezio84 <brabus84@gmail.com>
2015-05-30 14:22:34 +0200

    QS: add enable/disable LED tile [1/2]
    Change-Id: Id5e00af915c1d1d2b103895c3143b54001caeb31
    PS1: new vector icon thanks to Michele Amendolagine

--- src/com/android/settings/slim/qs/QSTileHolder.java
+++ src/com/android/settings/slim/qs/QSTileHolder.java
@@ -153,6 +153,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screenshot";
                 stringId = R.string.qs_tile_screenshot;
                 break;
+            case QSConstants.TILE_LED:
+                resourceName = "ic_qs_led_on";
+                stringId = R.string.qs_tile_led;
+                break;
             default:
                 return null;
         }

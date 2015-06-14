c9c69f7f23b57b26518ed12157fed51f41425d81
Victor Lapin <neonproject@gmail.com>
2015-06-15 01:02:53 +0300

    Made heads up switchable for 'system' packages
    Change-Id: I9c75fce25c07e5e02377ca95974255f13af682a0

--- src/com/android/settings/notification/AppNotificationSettings.java
+++ src/com/android/settings/notification/AppNotificationSettings.java
@@ -272,8 +272,8 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
 
         if (isSystemPkg) {
             prefs.removePreference(mBlock);
-            prefs.removePreference(mHeadsUp);
             mPriority.setDependency(null); // don't have it depend on a preference that's gone
+            mHeadsUp.setDependency(null);
         }
 
     }

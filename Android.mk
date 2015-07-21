5e742e3c5e0caacdbad9147d326f93c55c2608da
Ricardo Cerqueira <ricardo@cyngn.com>
2015-07-21 14:59:44 -0700

    Don't include all resource densities
    There's no reason for this anymore, trim out the drawable fat
    
    Change-Id: Ie8c4b7c226470c0120eac54c831287e54a4ed075

--- Android.mk
+++ Android.mk
@@ -34,8 +34,6 @@ LOCAL_AAPT_FLAGS := \
     --auto-add-overlay \
     --extra-packages android.support.v7.cardview
 
-LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
-
 include frameworks/opt/setupwizard/navigationbar/common.mk
 include $(BUILD_PACKAGE)
 

e3e7f4b0e1dc07222677b5cd2bdb86eb70c3cb93
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-08-26 00:33:34 +0200

    [1/2] Settings: Add lockscreen rotation as an optional rotation setting
    Based on :
    
    https://github.com/omnirom/android_packages_apps_Settings/commit/5a1b3a20277f7413aa51756de631db1a4235cfe9
    
    By :  Juan Ezquerro LLanes <arrase@gamil.com>
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    Change-Id: I925c3c2431a904ca4de15eca8d73308304c47fdc

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -106,6 +106,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_TAP_TO_WAKE = "double_tap_wake_gesture";
     private static final String CATEGORY_ADVANCED = "advanced_display_prefs";
 
+    private static final String ROTATION_LOCKSCREEN = "Lockscreen";
     private static final String ROTATION_ANGLE_0 = "0";
     private static final String ROTATION_ANGLE_90 = "90";
     private static final String ROTATION_ANGLE_180 = "180";
@@ -380,11 +381,19 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 DisplayRotation.ROTATION_0_MODE|DisplayRotation.ROTATION_90_MODE
                 |DisplayRotation.ROTATION_270_MODE);
 
+        boolean configEnableLockRotation = getResources().
+                        getBoolean(com.android.internal.R.bool.config_enableLockScreenRotation);
+        boolean lockScreenRotationEnabled = Settings.System.getInt(getContentResolver(),
+                        Settings.System.LOCKSCREEN_ROTATION, configEnableLockRotation ? 1 : 0) != 0;
+
         if (!rotationEnabled) {
             summary.append(getString(R.string.display_rotation_disabled));
         } else {
             ArrayList<String> rotationList = new ArrayList<String>();
             String delim = "";
+            if (lockScreenRotationEnabled) {
+                rotationList.add(ROTATION_LOCKSCREEN);
+            }
             if ((mode & DisplayRotation.ROTATION_0_MODE) != 0) {
                 rotationList.add(ROTATION_ANGLE_0);
             }

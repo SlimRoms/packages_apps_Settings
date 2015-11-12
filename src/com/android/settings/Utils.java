3d53e0aeaedb76eb4a2891f7fdc6fbb8b6dd07a5
penn <zhang_p.neu@codeaurora.org>
2015-11-12 09:54:00 +0800

    Settings: Fix for crashed in saved networks popup when rotate.
    Don't create new activity when rotate device.
    
    Change-Id: I7719dd1fdb66f579f8199b90db26057ac041c8ea
    CRs-Fixed: 937831

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -93,6 +93,7 @@ import com.android.settings.dashboard.DashboardTile;
 import com.android.settings.drawable.CircleFramedDrawable;
 import com.android.settingslib.applications.ApplicationsState;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -721,7 +722,10 @@ public final class Utils {
         if (BluetoothSettings.class.getName().equals(fragmentName)) {
             intent.setClass(context, SubSettings.BluetoothSubSettings.class);
             intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_AS_SUBSETTING, true);
-         } else {
+         } else if(SavedAccessPointsWifiSettings.class.getName().equals(fragmentName)) {
+            intent.setClass(context, SubSettings.SavedAccessPointsSubSettings.class);
+            intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_AS_SUBSETTING, true);
+        }else {
              intent.setClass(context, SubSettings.class);
          }
         intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT, fragmentName);

c3050a01d97a71cca88d2826f4389ad6e532bdd7
Danny Baumann <dannybaumann@web.de>
2015-12-27 19:52:39 +0100

    [2/4] Add increasing ring feature
    This also squashes in the following fixes:
    Fix NPE.
    Fix up increasing ring tone volume preview behaviour.
    Always set ramp-up time value during bindView
    
    Change-Id: I71ed1d0280ebda2cf28473d79fd4268dc9aa75b7

--- Android.mk
+++ Android.mk
@@ -37,7 +37,8 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v7/preference/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/recyclerview/res
+    frameworks/support/v7/recyclerview/res \
+    frameworks/opt/slim/preference/res
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
@@ -46,7 +47,7 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview
+    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview:org.slim.preference
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled

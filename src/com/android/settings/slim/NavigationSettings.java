8a40e8debd1b98e2d2c025d533d607be5a9402d6
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-01-19 01:16:52 +0100

    [2/2] Slim NavBar (squashed commit)
    patchset: org.slim, not com.margaritov
    
    Main work by Lars Greiss <kufikugel@googlemail.com>
    
    Contributions by (hopefully forgot noone) :
    
      - Griffin Millender <griffinn.millender@gmail.com>
      - Daniel Koman <dankoman30@gmail.com>
      - Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    Ported to AOSP MM by Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    Commit list follows :
    
    Settings: Slims navbar customizations
    
    patchset 14: fix reset button in button style screen
    
    patchset 15: CheckBoxPreference to SwitchPreference
    
    patchset 16-24: refer to correct reset icon
    
    patchset 21: fix navigation dashboard icon
    
    add back our navbar tweeks. This is a completly overworked
    version compared to the one we carry about a year now. Small glitches
    we had are now all fixed. As well it does not need to register extra observers
    code was much slimmed down and improved.
    Beside that we adapt the 4.4 navbar changes to our system so that it it
    consistent with the new 4.4 UI
    This patch adds:
    - Navbar up to 5 buttons
    - dynamic menu behaviour
    - landscape navbar options as well now on tablets
    - button glow color
    - button icon color
    - navrign and navbar user can set system or custom icons
    - navbar height and width
    - enable/disable navbar
    - actions are vor navbar and available for the whole system over SlimActions.java
    	back
    	custom app or activtiy
    	home
    	kill app
    	last app
    	notification drawer
    	ime switcher
    	power menu
    	qs pannel
    	recents screen
    	screen off
    	screenshot
    	search
    	torch if it is available on the device
    - added Color class based on blk_jacks idea which first grey an icon out
      and then color it. This gives way better results. As well it fits now
      perfectly with the new 4.4 UI. Put it on a central place so that it is
      from everywhere available. notification shortcuts adapted to the new class
      place
    - complete new icons thanks to kroz who has redone all for xxhdpi
    - our slim special swipe drag drop press longpress options support
      like on 4.3 this two features
    - a lot lot fixes as well on current google code
    Fix custom icons in navbar
    
    SlimActions: add menu big action
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Settings: add expanded desktop navbar action
    
    Fix nav checking / handling
    
    Fixup action list for Navbar
    Fix merge derp
    Add MetricsLogger methods
    Set NavBar enabled by default on clean install
    Fetch and set NavBar dimensions by default on clean install
    
    Change-Id: I175fe728742c6a2dc4e8db48d6e090e79c273417

---
+++ src/com/android/settings/slim/NavigationSettings.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2013-2016 SlimRoms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.ServiceManager;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.view.IWindowManager;
+
+import org.slim.framework.internal.logging.SlimMetricsLogger;
+import org.slim.provider.SlimSettings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NavigationSettings extends SettingsPreferenceFragment {
+
+    @Override
+    protected int getMetricsCategory() {
+        return SlimMetricsLogger.NAVIGATION_SETTINGS;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.slim_navigation_settings);
+    }
+
+}

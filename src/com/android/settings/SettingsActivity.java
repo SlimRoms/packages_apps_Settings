93ece45fcb0524db835b04a38a106b424cdcab8f
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-07-23 11:34:50 +0200

    [2/3] Settings: Statusbar clock and date customizations
    Settings: statusbar clock customization + initial interface + color picker
    
    - added slims clock and date customizations for status bar
    - added our own modded marginov color picker which we use as default
    - added initial interface settings. A bit empty currently but we are on
      alpha and it will be filled fast for sure. Added as well two empty
    classes
      due that we need them later
    
    Originally by kufikugel. I just ported to 5.0
    
    PS:
    - small cleanups and adaptions to LP
    - add personalization category on dash board.
    - go inline with new default values on fb patch
    - add correct dialog handling
    - use correct reset icon
    etc.
    
    Settings: Show clock enabled/disabled status summary in status bar settings (Lars Greiss)
    
    Settings: Add switch for clock color override, overrides statusbar tint (Yank555.lu)
    
    Settings: Adapt to make use of Slim framework (Yank555.lu)
    
    Settings: Add option for left clock style / position date left-right of clock (Yank555.lu)
    
      This adds :
    
      - clock positioned on the left
      - option to position date left or right of clock
    
    PS3 : change date position text to make it less confusing
    
    Change-Id: Iba18fc00c7499a01cff2ea74a0977eb5d33c764c
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -116,6 +116,7 @@ import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
 import com.android.settings.privacyguard.PrivacyGuardPrefs;
 import com.android.settings.sim.SimSettings;
+import com.android.settings.slim.InterfaceSettings;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.slim.NavigationSettings;
 import com.android.settings.users.UserSettings;
@@ -358,6 +359,7 @@ public class SettingsActivity extends Activity
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
             NavigationSettings.class.getName(),
+            InterfaceSettings.class.getName(),
     };
 
 

f2673c82779e9f44d134718388c8c0ffe150fcc0
Griffin Millender <griffinn.millender@gmail.com>
2016-07-28 01:49:29 -0500

    Move slim settings to SlimSettings in SlimFramework
    Change-Id: If066dd1318134e9d792c19278376f12fb82c02cb

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -116,9 +116,7 @@ import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
 import com.android.settings.privacyguard.PrivacyGuardPrefs;
 import com.android.settings.sim.SimSettings;
-import com.android.settings.slim.InterfaceSettings;
 import com.android.settings.tts.TextToSpeechSettings;
-import com.android.settings.slim.NavigationSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.vpn2.VpnSettings;
 import com.android.settings.wfd.WifiDisplaySettings;
@@ -357,8 +355,6 @@ public class SettingsActivity extends Activity
             ProcessStatsSummary.class.getName(),
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
-            NavigationSettings.class.getName(),
-            InterfaceSettings.class.getName(),
     };
 
 

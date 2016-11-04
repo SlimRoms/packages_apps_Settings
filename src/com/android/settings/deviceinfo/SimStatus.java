446a6460094ea48971b0db1b87cd23875aed0318
Benergy Meenan Ravuri <bravuri@codeaurora.org>
2016-11-04 15:29:17 +0530

    IMS: Cellular network type is not showing VoLTE enabled
    Cellular network type in SIM status is not showing
    VoLTE enabled if VoLTE SIM is inserted in Second subscription.
    
    Check IMS registration status per subscription.
    
    Change-Id: I2210c9ce99871a0188798c5e7eaba2f37f282d13
    CRs-Fixed: 1085544

--- src/com/android/settings/deviceinfo/SimStatus.java
+++ src/com/android/settings/deviceinfo/SimStatus.java
@@ -62,6 +62,7 @@ import java.util.List;
 import static android.content.Context.CARRIER_CONFIG_SERVICE;
 import static android.content.Context.TELEPHONY_SERVICE;
 
+import org.codeaurora.ims.utils.QtiImsExtUtils;
 
 /**
  * Display the following information
@@ -289,12 +290,10 @@ public class SimStatus extends SettingsPreferenceFragment {
             networktype = "4G";
         }
 
-        String property = SystemProperties.get("persist.radio.atel.carrier");
-        boolean isCarrierOneSupported = "405854".equals(property);
-        if (isCarrierOneSupported) {
+        if (QtiImsExtUtils.isCarrierOneSupported()) {
             if (TelephonyManager.NETWORK_TYPE_LTE == actualDataNetworkType ||
                     TelephonyManager.NETWORK_TYPE_LTE == actualVoiceNetworkType) {
-                if (mTelephonyManager.isImsRegistered()) {
+                if (mTelephonyManager.isImsRegisteredForSubscriber(subId)) {
                     networktype = getResources().
                             getString(R.string.lte_data_and_voice_calling_enabled);
                 } else {
